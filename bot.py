bfbbb62291e5d19fb7a12aab1472f00e63d55a99
Saywex89 <58743402+Saywex89@users.noreply.github.com>
2020-06-22 21:08:41 +0300

    rename prefix

--- bot.py
+++ bot.py
@@ -6,15 +6,15 @@ from discord.ext import commands
 import pyowm
 
 TOKEN = 'NzI0NjQyNjgwMTI2MDQ2MzM5.XvDbTg.7tmEJ94cyQtJIsEqw7aMbowsNSg'
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='>')
 
+bot.remove_command('help')
 
 @bot.command(pass_context=True)  
-async def weather(ctx, arg):
-
-
+async def help(ctx, arg):
     await ctx.send(arg)  
 
 
 bot.run(TOKEN)
+
 
