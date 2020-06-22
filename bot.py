f98e0d55cbc462d3c7cb82c9eeb15e38415ee9b2
Saywex89 <58743402+Saywex89@users.noreply.github.com>
2020-06-22 20:43:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,13 +3,16 @@ import asyncio
 import os
 import discord
 from discord.ext import commands
+import pyowm
 
 TOKEN = 'NzI0NjQyNjgwMTI2MDQ2MzM5.XvDbTg.7tmEJ94cyQtJIsEqw7aMbowsNSg'
 bot = commands.Bot(command_prefix='!')
 
 
 @bot.command(pass_context=True)  
-async def test(ctx, arg):  
+async def weather(ctx, arg):
+
+
     await ctx.send(arg)  
 
 
