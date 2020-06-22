ad5be5b039b8084c50f3e3acc21e171a46bd3a58
Saywex89 <58743402+Saywex89@users.noreply.github.com>
2020-06-22 19:22:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,12 @@
 import discord                                            
 from discord.ext import commands
                
-token = 'g6ii8K1Gsd6JxZrtuc9Mf1GfDdaLq7k'
-
 bot = commands.Bot(command_prefix='>')   
 
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user.name}')
-
 @bot.command()
-async def (ctx: commands.Context):
-    await ctx.send('Pong!')
+async def commands(ctx):
+    await ctx.send('тест')
 
 
-bot.run(token=token)                             
+bot.run(token='g6ii8K1Gsd6JxZrtuc9Mf1GfDdaLq7k')                             
+       
