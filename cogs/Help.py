00536487bb4db7a9f8c2b23a7093eeeaf339850e
Saywex89 <58743402+Saywex89@users.noreply.github.com>
2020-08-21 02:12:15 +0300

    fix help cog

--- cogs/Help.py
+++ cogs/Help.py
@@ -9,8 +9,11 @@ class Help(commands.Cog):
        
     @commands.command()
     async def help(self, ctx):
-        await ctx.message.delete()
-        help_channel = discord.utils.get(ctx.author.guild.channels, id=745819212320342026)
-        await ctx.send(f'{ctx.author.mantion} почитать правила можете тут {help_channel}.')
+        try:
+            await ctx.message.delete()
+            help_channel = discord.utils.get(ctx.author.guild.channels, id=745819212320342026)
+            await ctx.send(embed = discord.Embed(description = f'{ctx.author.mention} почитать правила можете тут {help_channel.mention}.', color=0x9d61e7))
+        except Exception as error:
+            print(error)
 def setup(bot):
     bot.add_cog(Help(bot))  
